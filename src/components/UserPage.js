618e50acd4475e90366b9561b168bb1e8a4ef748
Adie Williams <hello@adie.codes>
2018-03-18 17:14:27 +0000

    feat: Add propTypes to UserPage

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PT from 'prop-types';
 
 const UserPage = props => (
   <div className="userPage">
     <h1>{props.user.name}</h1>
   </div>
 );
+
+UserPage.propTypes = {
+  user: PT.object.isRequired
+};
 
 export default UserPage;
