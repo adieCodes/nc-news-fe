61333f0cc131dff495da1e74c534f9912e49354d
Adie Williams <hello@adie.codes>
2018-03-18 17:10:32 +0000

    feat: Add propTypes to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import NavItem from './NavItem';
+import PT from 'prop-types';
 
 const Nav = props => (
   <ul className="nav">{props.topics.map(topic => <NavItem key={topic._id}>{topic}</NavItem>)}</ul>
 );
+
+Nav.propTypes = {
+  topics: PT.array.isRequired
+};
 
 export default Nav;
