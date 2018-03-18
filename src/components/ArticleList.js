6f4a385bd10eca98cf8e8c7740c4cb32789ebc5d
Adie Williams <hello@adie.codes>
2018-03-18 17:04:37 +0000

    feat: Add propType check to ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ArticleCard from './ArticleCard';
+import PT from 'prop-types';
 
 const ArticleList = props => {
   return (
@@ -8,6 +9,10 @@ const ArticleList = props => {
       {props.articles.map(article => <ArticleCard article={article} key={article._id} />)}
     </div>
   );
+};
+
+ArticleList.propTypes = {
+  articles: PT.array.isRequired
 };
 
 export default ArticleList;
