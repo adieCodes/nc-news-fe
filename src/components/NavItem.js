0e6f4a25eff94e291ca6970c7da322ad0d0b425d
Adie Williams <hello@adie.codes>
2018-03-18 17:11:54 +0000

    feat: Add propTypes to NavItem

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PT from 'prop-types';
 
 const NavItem = ({ children }) => (
   <li className="navItem">
     <NavLink to={`/topics/${children.slug}`}>{children.title}</NavLink>
   </li>
 );
+
+NavItem.propTypes = {
+  children: PT.object.isRequired
+};
 
 export default NavItem;
