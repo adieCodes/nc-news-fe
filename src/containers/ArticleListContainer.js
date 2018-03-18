6d21de51eb39e26ad6bc6a88e9a8007b38e61cc8
Adie Williams <hello@adie.codes>
2018-03-18 16:38:47 +0000

    feat: Add lifecycle method to ensure re-render

--- src/containers/ArticleListContainer.js
+++ src/containers/ArticleListContainer.js
@@ -3,11 +3,21 @@ import { getAllArticles } from '../api';
 import ArticleList from '../components/ArticleList';
 
 class ArticleListContainer extends Component {
-  state = { articles: [] };
+  state = { articles: [], location: '' };
+
+  fetchArticles() {
+    const topicRequest = !this.props.match.params.topicId ? '' : this.props.match.params.topicId;
+    getAllArticles(topicRequest).then(res =>
+      this.setState({ articles: res.articles, location: this.props.location })
+    );
+  }
 
   componentDidMount() {
-    const topicRequest = !this.props.match.params.topicId ? '' : this.props.match.params.topicId;
-    getAllArticles(topicRequest).then(res => this.setState({ articles: res.articles }));
+    this.fetchArticles();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.location !== this.props.location) this.fetchArticles();
   }
 
   render() {
