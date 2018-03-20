cd0dd61f6a436854f1c444cb07a221a7f8025f4c
Adie Williams <hello@adie.codes>
2018-03-20 16:31:49 +0000

    feat: Add Link to Home page in Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import NavItem from './NavItem';
 import PT from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Nav = props => (
-  <ul className="nav">{props.topics.map(topic => <NavItem key={topic._id}>{topic}</NavItem>)}</ul>
+  <ul className="nav">
+    <li className="navItem">
+      <Link to="/">Home</Link>
+    </li>
+    {props.topics.map(topic => <NavItem key={topic._id}>{topic}</NavItem>)}
+  </ul>
 );
 
 Nav.propTypes = {
