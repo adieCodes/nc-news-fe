95db943411f93e45aeec3f0afee15ee511493c79
Adie Williams <hello@adie.codes>
2018-03-24 10:05:02 +0000

    feat: Add Link to UserPage from CommentCard

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -6,7 +6,9 @@ const CommentCard = props => {
   return (
     <div className="comment-card">
       <p>{props.comment.body}</p>
-      <p>{props.comment.created_by}</p>
+      <p>
+        <Link to={`/users/${props.comment.created_by}`}>{props.comment.created_by}</Link>
+      </p>
       <p>{props.comment.created_at}</p>
       <div class="article-card__voting-buttons">
         <Link to={'#'}>Up</Link>
