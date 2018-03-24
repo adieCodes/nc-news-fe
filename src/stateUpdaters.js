8485ed2791ddefb37ec2823291c99498d55bbb01
Adie Williams <hello@adie.codes>
2018-03-24 12:58:36 +0000

    feat: articleVote stateUpdater ignores invalid voteTypes and doesn't mutate state

--- src/stateUpdaters.js
+++ src/stateUpdaters.js
@@ -1,4 +1,5 @@
 const articleVote = (articles, articleId, voteType) => {
+  if (!/^down$|^up$/.test(voteType)) return articles.slice(0);
   const voteValue = voteType === 'up' ? 1 : -1;
   return articles.map(article => {
     if (article._id === articleId) {
