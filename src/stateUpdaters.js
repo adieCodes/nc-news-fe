7d286d338d9696fee0d98c0628ede2b6a4e1ef0e
Adie Williams <hello@adie.codes>
2018-03-24 12:46:15 +0000

    feat: articleVote increments vote on a single article

--- src/stateUpdaters.js
+++ src/stateUpdaters.js
@@ -1,9 +1,9 @@
 const articleVote = (articles, articleId, voteType) => {
   const voteValue = voteType === 'up' ? 1 : -1;
   return articles.map(article => {
-    if (article._id === articleId)
+    if (article._id === articleId) {
       return Object.assign({}, article, { votes: article.votes + voteValue });
-    else return article;
+    } else return article;
   });
 };
 
