89ebd42965ad3e3949f249a7a7fdc9f2efab5df6
Adie Williams <hello@adie.codes>
2018-03-25 12:02:20 +0100

    feat: Limit voting to ensure no-one can add/remove more than 1 vote

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,45 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const ArticleCard = props => {
-  return (
-    <div className="article-card">
-      <h2>
-        <Link to={`/topics/${props.article.belongs_to}/${props.article._id}`}>
-          {props.article.title}
-        </Link>
-      </h2>
-      <Link to={`/topics/${props.article.belongs_to}`}>{props.article.belongs_to}</Link>
-      <Link to={`/users/${props.article.created_by}`}>{props.article.created_by}</Link>
-      <div className="article-card__voting-buttons">
-        <Link
-          to={'#'}
-          onClick={event => {
-            const articleId = props.article._id;
+class ArticleCard extends Component {
+  state = {
+    voteChange: 0,
+    voteUpDisabled: false,
+    voteDownDisabled: false
+  };
 
-            event.preventDefault();
-            return props.handleVote(articleId, 'up');
-          }}
-        >
-          Up
-        </Link>
-        <span>{props.article.votes}</span>
-        <Link
-          to={'#'}
-          onClick={event => {
-            const articleId = props.article._id;
+  voteUp = event => {
+    const articleId = this.props.article._id;
+    const newVoteChange = this.state.voteChange + 1;
+    const blockVoteUp = newVoteChange > 0;
+    const blockVoteDown = newVoteChange < 1;
 
-            event.preventDefault();
-            return props.handleVote(articleId, 'down');
-          }}
-        >
-          Down
-        </Link>
+    event.preventDefault();
+    this.setState({
+      voteChange: newVoteChange,
+      voteUpDisabled: blockVoteUp,
+      voteDownDisabled: blockVoteDown
+    });
+    return this.props.handleVote(articleId, 'up');
+  };
+  voteDown = event => {
+    const articleId = this.props.article._id;
+    const newVoteChange = this.state.voteChange - 1;
+    const blockVoteUp = newVoteChange > 1;
+    const blockVoteDown = newVoteChange < 0;
+
+    event.preventDefault();
+
+    this.setState({
+      voteChange: newVoteChange,
+      voteUpDisabled: blockVoteUp,
+      voteDownDisabled: blockVoteDown
+    });
+    return this.props.handleVote(articleId, 'down');
+  };
+
+  render() {
+    return (
+      <div className="article-card">
+        <h2>
+          <Link to={`/topics/${this.props.article.belongs_to}/${this.props.article._id}`}>
+            {this.props.article.title}
+          </Link>
+        </h2>
+        <Link to={`/topics/${this.props.article.belongs_to}`}>{this.props.article.belongs_to}</Link>
+        <Link to={`/users/${this.props.article.created_by}`}>{this.props.article.created_by}</Link>
+        <div className="article-card__voting-buttons">
+          <button onClick={this.voteUp} disabled={this.state.voteUpDisabled}>
+            Up
+          </button>
+          <span>{this.props.article.votes}</span>
+          <button onClick={this.voteDown} disabled={this.state.voteDownDisabled}>
+            Down
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 ArticleCard.propTypes = {
   article: PT.object.isRequired
