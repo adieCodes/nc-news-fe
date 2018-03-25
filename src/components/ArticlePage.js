0a784b54c517967970baae4f8a535720a1d5a628
Adie Williams <hello@adie.codes>
2018-03-25 16:26:42 +0100

    feat: Add voting on ArticlePage

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import CommentCard from './CommentCard';
+import VoteButton from './VoteButton';
+import { limitVote } from '../stateUpdaters';
 
-const ArticlePage = props => {
-  return (
-    <div className="articlePage">
-      <h1>{props.article.title}</h1>
-      <Link to={`/topics/${props.article.belongs_to}`}>{props.article.belongs_to}</Link>
-      <Link to={`/users/${props.article.created_by}`}>{props.article.created_by}</Link>
-      <div class="article-card__voting-buttons">
-        <Link to={'#'}>Up</Link>
-        <span>{props.article.votes}</span>
-        <Link to={'#'}>Down</Link>
+class ArticlePage extends Component {
+  state = {
+    voteChangedBy: 0,
+    voteUpDisabled: false,
+    voteDownDisabled: false
+  };
+
+  vote = (event, voteType) => {
+    const articleId = this.props.article._id;
+    const newState = limitVote(this.state, voteType);
+
+    this.setState(newState);
+    this.props.handleVote(articleId, voteType);
+  };
+
+  render() {
+    return (
+      <div className="articlePage">
+        <h1>{this.props.article.title}</h1>
+        <Link to={`/topics/${this.props.article.belongs_to}`}>{this.props.article.belongs_to}</Link>
+        <Link to={`/users/${this.props.article.created_by}`}>{this.props.article.created_by}</Link>
+        <div className="article-card__voting-buttons">
+          <VoteButton vote={this.vote} voteType="up" activeState={this.state.voteUpDisabled} />
+          <span>{this.props.article.votes}</span>
+          <VoteButton vote={this.vote} voteType="down" activeState={this.state.voteDownDisabled} />
+        </div>
+        <article className="article">{this.props.article.body}</article>
+        {this.props.comments.map(comment => <CommentCard comment={comment} />)}
       </div>
-      <article className="article">{props.article.body}</article>
-      {props.comments.map(comment => <CommentCard comment={comment} />)}
-    </div>
-  );
-};
+    );
+  }
+}
 
 ArticlePage.propTypes = {
   article: PT.object.isRequired,
-  comments: PT.array.isRequired
+  comments: PT.array.isRequired,
+  handleVote: PT.func.isRequired
 };
 
 export default ArticlePage;
--- src/containers/ArticlePageContainer.js
+++ src/containers/ArticlePageContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { getArticleById, getCommentsByArticle } from '../api';
 import ArticlePage from '../components/ArticlePage';
 
+import { articleVote } from '../stateUpdaters';
+
 class ArticlePageContainer extends Component {
   state = { article: {}, comments: [] };
 
@@ -11,8 +13,21 @@ class ArticlePageContainer extends Component {
     getCommentsByArticle(articleId).then(res => this.setState({ comments: res.comments }));
   }
 
+  handleVote = (articleId, voteType) => {
+    const { article } = this.state;
+    const newArticles = articleVote(article, articleId, voteType);
+
+    this.setState({ article: newArticles });
+  };
+
   render() {
-    return <ArticlePage article={this.state.article} comments={this.state.comments} />;
+    return (
+      <ArticlePage
+        article={this.state.article}
+        comments={this.state.comments}
+        handleVote={this.handleVote}
+      />
+    );
   }
 }
 
