4deac2fc48ec72f7e737bfa67eed1db48c44a056
Adie Williams <hello@adie.codes>
2018-03-30 08:55:43 +0100

    fix: Replace <p> wrapping emojies with <span> and attributes

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Loading = props => (
   <div className="loading">
-    <p>🤔 🤔 🤔 🤔</p>
+    <span role="img" aria-label="emoji thinking faces display while loading">
+      🤔 🤔 🤔 🤔
+    </span>
   </div>
 );
 
