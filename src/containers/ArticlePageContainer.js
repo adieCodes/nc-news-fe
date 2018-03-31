63d50abc129b2f55c5cf1985eea5e6fb869525fb
Adie Williams <hello@adie.codes>
2018-03-31 13:50:04 +0100

    feat: Add prop-type validation to ArticlePageContainer

--- src/containers/ArticlePageContainer.js
+++ src/containers/ArticlePageContainer.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import PT from 'prop-types';
 import {
   getArticleById,
   getCommentsByArticle,
@@ -65,5 +66,9 @@ class ArticlePageContainer extends Component {
     );
   }
 }
+
+ArticlePageContainer.propTypes = {
+  match: PT.object.isRequired
+};
 
 export default ArticlePageContainer;
