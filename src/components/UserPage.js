9bda56f047a85849443ee22590c090b568d00903
Adie Williams <hello@adie.codes>
2018-04-01 20:22:09 +0100

    style: Add styling for UserPage

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -2,9 +2,20 @@ import React from 'react';
 import PT from 'prop-types';
 
 const UserPage = props => (
-  <div className="userPage">
-    <h1>{props.user.name}</h1>
-    <img src={props.user.avatar_url} alt={`profile shot for ${props.user.name}`} />
+  <div className="userPage container column is-two-thirds">
+    <div className="card">
+      <div className="card-content">
+        <div className="media">
+          <div className="media-center">
+            <img src={props.user.avatar_url} alt={`profile shot for ${props.user.name}`} />
+          </div>
+          <div className="media-content">
+            <p className="title userPage-title">{props.user.name}</p>
+            <p className="subtitle is-6 userPage-subtitle">@{props.user.username}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
