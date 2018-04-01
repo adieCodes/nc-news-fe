a313983a2805350e89baa91d328ce13789c9ed4a
Adie Williams <hello@adie.codes>
2018-04-01 20:30:21 +0100

    style: Add base styles for UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const UserList = () => <h1>User List</h1>;
+const UserList = () => (
+  <div className="userList container column is-two-thirds">
+    <div className="card">
+      <div className="card-content">
+        <div className="media">
+          <div className="media-center">
+            <p className="title userList-title">User List</p>
+            <ul>
+              <li>user list coming soon...</li>
+              <li>probably</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default UserList;
