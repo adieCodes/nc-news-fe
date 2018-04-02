0f89d2747f2a76755029bae86d41ee5534891cbb
Adie Williams <hello@adie.codes>
2018-04-02 15:01:52 +0100

    feat: Add prop for enabling voting

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,15 +6,25 @@ const ArticleList = props => (
   <div className="articleList container">
     <div className="columns is-multiline is-centered">
       {props.articles.map(article => (
-        <ArticleCard article={article} key={article._id} handleVote={props.handleVote} />
+        <ArticleCard
+          article={article}
+          key={article._id}
+          handleVote={props.handleVote}
+          votingActive={props.votingActive}
+        />
       ))}
     </div>
   </div>
 );
 
+ArticleCard.defaultProps = {
+  votingActive: true
+};
+
 ArticleList.propTypes = {
   articles: PT.array.isRequired,
-  handleVote: PT.func.isRequired
+  handleVote: PT.func.isRequired,
+  votingActive: PT.bool
 };
 
 export default ArticleList;
