1067e25c4fb8cf7dfefc8d553b0b7b8a261c0354
Adie Williams <hello@adie.codes>
2018-04-02 15:14:29 +0100

    Issue18add user articles (#44)
    * refactor: To use new data from /api/users/:username
    
    * feat: Add conditional rendering for when voting is enabled
    
    * feat: Add prop for enabling voting
    
    * feat: Render articles without voting
    
    * feat: Add article count and hash link
    
    * test: Make handleVote optional in ArticleList and ArticleCard

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import PT from 'prop-types';
+import ArticleList from '../components/ArticleList';
 
 const UserPage = props => (
-  <div className="userPage container column is-two-thirds">
+  <div className="userPage container">
     <div className="card">
       <div className="card-content">
         <div className="media">
           <div className="media-center">
-            <img src={props.user.avatar_url} alt={`profile shot for ${props.user.name}`} />
+            <img src={props.userData.avatar_url} alt={`profile shot for ${props.userData.name}`} />
           </div>
           <div className="media-content">
-            <p className="title userPage-title">{props.user.name}</p>
-            <p className="subtitle is-6 userPage-subtitle">@{props.user.username}</p>
+            <p className="title userPage-title">{props.userData.name}</p>
+            <p className="subtitle is-6 userPage-subtitle">@{props.userData.username}</p>
+            <a href="#articles">
+              <p>
+                <strong>{props.userArticles.length}</strong> Articles
+              </p>
+            </a>
           </div>
         </div>
       </div>
+    </div>
+    <div className="articleList" id="articles">
+      <ArticleList articles={props.userArticles} votingActive={false} />;
     </div>
   </div>
 );
 
 UserPage.propTypes = {
-  user: PT.object.isRequired
+  userData: PT.object.isRequired,
+  userArticles: PT.array.isRequired,
+  userComments: PT.array.isRequired
 };
 
 export default UserPage;
